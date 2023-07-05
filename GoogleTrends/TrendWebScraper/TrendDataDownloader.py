bf1da51c6d38911eaedc6b47536552b14a68f50e
Fiachra <fiachraf12@gmail.com>
2023-07-06 00:25:46 +0100

    created a scraper for the IMDB website

--- GoogleTrends/TrendWebScraper/TrendDataDownloader.py
+++ GoogleTrends/TrendWebScraper/TrendDataDownloader.py
@@ -7,38 +7,91 @@ thisPath = pathlib.Path(__file__).parent.resolve()
 # by default just use test as the first search term
 trendTerm = 'test'
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.firefox.launch(headless=False)
-    #commented out as new context creates it with no cookies
-    #context = browser.new_context()
-    #page = context.new_page()
-    page = browser.new_page()
-    # try get some cookies so that google trends doesn't block me
-    #context.cookies()
-    page.goto("https://docs.python.org/3/reference/datamodel.html")
+# def run(playwright: Playwright) -> None:
+#
+#     #commented out as new context creates it with no cookies
+#     #context = browser.new_context()
+#     #page = context.new_page()
+#
+#     # try get some cookies so that google trends doesn't block me
+#     #context.cookies()
+#     page.goto("https://docs.python.org/3/reference/datamodel.html")
+#
+#     #seems that google is able to detect my bot and I need to click refresh and then it will continue for at least 2 search terms
+#
+#     #go to first page
+#     # google trends urls have a pattern, this one looks for data from all time, worldwide
+#     page.goto("https://trends.google.com/trends/explore?date=all&q=test&hl=en-GB")
+#     #click the download csv file
+#     with page.expect_download() as download_info:
+#         page.locator("widget").filter(has_text="Interest over time help_outline Help file_download code share ‪1 Jan 2004‬‪1 Oct").get_by_role("button", name="file_download").click()
+#     download = download_info.value
+#     download.save_as(f'{thisPath}/{trendTerm}.csv')
+#     # click the search term box, type in the term 'help', press Enter then download the csv again
+#     page.get_by_role("searchbox", name="Add a search term").click()
+#     page.get_by_role("searchbox", name="Add a search term").fill("help")
+#     page.get_by_role("searchbox", name="Add a search term").press("Enter")
+#     with page.expect_download() as download1_info:
+#         page.locator("widget").filter(has_text="Interest over time help_outline Help file_download code share ‪1 Jan 2004‬‪1 Oct").get_by_role("button", name="file_download").click()
+#     download1 = download1_info.value
+#
+#     # ---------------------
+#     context.close()
+#     browser.close()
 
-    #seems that google is able to detect my bot and I need to click refresh and then it will continue for at least 2 search terms
-
+def gotoGTrend(GTrendPage):
     #go to first page
     # google trends urls have a pattern, this one looks for data from all time, worldwide
-    page.goto("https://trends.google.com/trends/explore?date=all&q=test&hl=en-GB")
+    GTrendPage.goto("https://trends.google.com/trends/explore?date=all&q=test&hl=en-GB")
+    #As the page is being passed as an object between the functions I shouldn't have to return anything
+    if GTrendPage.get_by_text("That's an error").count() > 0:
+        print('Error occured, waiting for human intervention')
+        dummy = input('check web browser, maybe it needs to be refreshed')
+    return None
+
+def dlTrendCSV(GTrendPage, thisPath, trendTerm):
     #click the download csv file
-    with page.expect_download() as download_info:
-        page.locator("widget").filter(has_text="Interest over time help_outline Help file_download code share ‪1 Jan 2004‬‪1 Oct").get_by_role("button", name="file_download").click()
+    with GTrendPage.expect_download() as download_info:
+        GTrendPage.locator("widget").filter(has_text="Interest over time help_outline Help file_download code share ‪1 Jan 2004‬‪1 Oct").get_by_role("button", name="file_download").click()
     download = download_info.value
     download.save_as(f'{thisPath}/{trendTerm}.csv')
+    return None
+
+def searchNewTrend(GTrendPage, newTerm):
     # click the search term box, type in the term 'help', press Enter then download the csv again
-    page.get_by_role("searchbox", name="Add a search term").click()
-    page.get_by_role("searchbox", name="Add a search term").fill("help")
-    page.get_by_role("searchbox", name="Add a search term").press("Enter")
-    with page.expect_download() as download1_info:
-        page.locator("widget").filter(has_text="Interest over time help_outline Help file_download code share ‪1 Jan 2004‬‪1 Oct").get_by_role("button", name="file_download").click()
-    download1 = download1_info.value
+    GTrendPage.get_by_role("searchbox", name="Add a search term").click()
+    GTrendPage.get_by_role("searchbox", name="Add a search term").fill("help")
+    GTrendPage.get_by_role("searchbox", name="Add a search term").press("Enter")
 
-    # ---------------------
-    context.close()
-    browser.close()
+    if GTrendPage.get_by_text("That's an error").count() > 0:
+        print('Error occured, waiting for human intervention')
+        dummy = input('check web browser, maybe it needs to be refreshed')
+    # page.get_by_text("429. That’s an error.").click()
+    # page.get_by_text("429.").click()
+    # page.get_by_text("That’s an error.").click()
+    return None
+
+def main():
+    with sync_playwright() as playwright:
+        #run(playwright)
+        #create a headed browser so I can click refresh so it won't think I'm a bot
+        browser = playwright.firefox.launch(headless=False)
+        page = browser.new_page()
+        #gets the path of this file
+        thisPath = pathlib.Path(__file__).parent.resolve()
+        # by default just use test as the first search term
+        trendTerm = 'test'
+        trendTerm2 = 'chicken'
+        gotoGTrend(page)
+        # #force it to wait for me to reload the page and then enter anything
+        # dummy = input('refresh the page and then press enter here')
+        dlTrendCSV(page, thisPath, trendTerm)
+        searchNewTrend(page, trendTerm2)
+        dlTrendCSV(page, thisPath, trendTerm2)
+
+        browser.close()
+        return None
 
 
-with sync_playwright() as playwright:
-    run(playwright)
+if __name__ == '__main__':
+    main()
---
+++ IMDB/IMDBScraper.py
@@ -0,0 +1,113 @@
+
+from playwright.sync_api import Playwright, sync_playwright, expect
+import pathlib
+from bs4 import BeautifulSoup
+import re
+
+#gets the path of this file
+thisPath = pathlib.Path(__file__).parent.resolve()
+# by default just use test as the first search term
+trendTerm = 'test'
+
+def main() -> None:
+    with sync_playwright() as playwright:
+        #create a headed browser so I can click refresh so it won't think I'm a bot
+        browser = playwright.firefox.launch(headless=False)
+        page = browser.new_page()
+
+        #try get the list from the top 250 page, it will fail if it loads the new imdb wesite and so it will keeo trying until it loads the old one
+        # try:
+            # linkList = imdbTop250(page)
+        # except Exception as e:
+        #     new_page = True
+        #     #keep trying to load old page
+        #     while new_page:
+        #         page.close()
+        #         page = browser.new_page()
+        #         try:
+        #             linkList = imdbTop250(page)
+        #             new_page = False
+        #         except Exception as e:
+        #             pass
+        # print(linkList)
+        # linkList = imdbTop250(page)
+        try:
+            linkList = imdbTop250New(page)
+        except Exception as e:
+            linkList = imdbTop250(page)
+        print(linkList)
+
+        #get the title of the movie from the webpage
+        mvTitle = movieTitle(page, 'https://www.imdb.com/title/tt0111161/')
+
+        print(mvTitle)
+
+        browser.close()
+
+
+def imdbTop250(Top250Page: Playwright) -> list:
+    # go to top 250 movies Top250Page to get the list of 250 urls to them do some meddling with to get 5000 movie titles
+    Top250Page.goto("https://www.imdb.com/chart/top/?ref_=nv_mv_250")
+    #this waits for the specified element to be loaded on the Top250Page. This will ensure that the table is loaded. If it doesn't load it will timeout and crash
+    # get it to close and open another tab if it opens the new imdb website
+    lastEntry=Top250Page.get_by_role("cell", name=re.compile(r"^250\."))
+    lastEntry.wait_for()
+    #get the html from the imdb page and then use beautiful soup to parse it and find the desired info
+    html = Top250Page.content()
+    soup = BeautifulSoup(html, 'html.parser')
+    #finds all the rows in the table that contain the movie title as well as part of the link for that movie on imdb
+    row_entries = soup.find_all(class_='titleColumn')
+    websiteList = []
+    #for each row
+    for k in row_entries:
+        #find the a tag in the row, is of type <class 'bs4.element.Tag'>
+        ape = k.find('a')
+        #get the partial link is of type <class 'str'>
+        partial_link = ape.get('href')
+        #fix link
+        link = 'https://www.imdb.com' + partial_link[:17]
+        #get the text/title, is of type <class 'str'>
+        mvTitle = ape.get_text()
+        # print(f'link: {link}\nmvTitle: {mvTitle}')
+        websiteList.append(link)
+    return list
+
+def imdbTop250New(Top250Page: Playwright) -> list:
+    # go to top 250 movies Top250Page to get the list of 250 urls to them do some meddling with to get 5000 movie titles
+    Top250Page.goto("https://www.imdb.com/chart/top/?ref_=nv_mv_250")
+    #this waits for the specified element to be loaded on the Top250Page. This will ensure that the table is loaded. If it doesn't load it will timeout and crash
+    # get it to close and open another tab if it opens the new imdb website
+    lastEntry=Top250Page.get_by_role("listitem").filter(has_text=re.compile(r"^250\."))
+    lastEntry.wait_for()
+    #get the html from the imdb page and then use beautiful soup to parse it and find the desired info
+    html = Top250Page.content()
+    soup = BeautifulSoup(html, 'html.parser')
+    row_entries = soup.find_all(class_='ipc-title-link-wrapper')
+    websiteList = []
+    for k in row_entries:
+        #k has type <class 'bs4.element.Tag'>
+        partial_link = k.get('href')
+        link = 'https://www.imdb.com' + partial_link[:17]
+        if link.find('title') == -1:
+            continue
+        #k has sub tag of h3 that has the movie title
+        grape = k.find('h3')
+        grapeText = grape.get_text()
+        mvTitle = grapeText[grapeText.find(' ') + 1:]
+        # print(f'link: {link}\nmvTitle: {mvTitle}')
+        websiteList.append(link)
+    return websiteList
+
+def movieTitle(mvPage, url) -> str:
+    #get the title of the movie from the webpage
+    mvPage.goto(url)
+    #get the html from the imdb page and then use beautiful soup to parse it and find the desired info
+    html = mvPage.content()
+    soup = BeautifulSoup(html, 'html.parser')
+    mvTitle = soup.find('title').get_text()
+    titleEndIndex = (re.search(r"\s*\(....\)", mvTitle)).span()[0]
+    return mvTitle[:titleEndIndex]
+
+
+if __name__ == '__main__':
+    main()
